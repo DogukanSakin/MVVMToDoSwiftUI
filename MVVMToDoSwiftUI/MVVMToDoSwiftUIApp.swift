645ff8860a4f94683ea0f181d4308222aea4be68
Doğukan Sakin <dogukansakin1@hotmail.com>
2024-10-31 20:47:00 +0300

    Initial Commit

---
+++ MVVMToDoSwiftUI/MVVMToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMToDoSwiftUIApp.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
