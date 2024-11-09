728cfe5200a714d5d804d82689b2f695db7a64b5
Doğukan Sakin <dogukansakin1@hotmail.com>
2024-11-09 19:46:38 +0300

    added base plus sheet, completed todo card

---
+++ MVVMToDoSwiftUI/Components/Cards/CompletedTodoCard.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedTodoCard.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 9.11.2024.
+//
+
+import SwiftUI
+
+struct CompletedTodoCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedTodoCard()
+}
---
+++ MVVMToDoSwiftUI/Utilities/Extensions/Formatter+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Formatter+Extension.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 9.11.2024.
+//
+
+import Foundation
