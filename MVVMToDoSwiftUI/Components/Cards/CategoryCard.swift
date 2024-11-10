9002a9c29915abd64cf5e0d3c0cc8878eb170199
Doğukan Sakin <dogukansakin1@hotmail.com>
2024-11-10 16:09:03 +0300

    added category card and new category form

---
+++ MVVMToDoSwiftUI/Components/Cards/CategoryCard.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryCard.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 10.11.2024.
+//
+
+import SwiftUI
+
+struct CategoryCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryCard()
+}
---
+++ MVVMToDoSwiftUI/ViewModels/CategoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryViewModel.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 10.11.2024.
+//
+
+import Foundation
