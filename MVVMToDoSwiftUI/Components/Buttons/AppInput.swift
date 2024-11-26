423bb303ea93d3bdbb1d58bdee75cf3a838a302f
Doğukan Sakin <dogukansakin1@hotmail.com>
2024-11-26 22:27:54 +0300

    added app base input component, added todo form

---
+++ MVVMToDoSwiftUI/Components/Buttons/AppInput.swift
@@ -0,0 +1,18 @@
+//
+//  AppInput.swift
+//  MVVMToDoSwiftUI
+//
+//  Created by Doğukan Sakin on 26.11.2024.
+//
+
+import SwiftUI
+
+struct AppInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppInput()
+}
